8ab7e933517fe6fa28d136144b03992cdf4909d4
Emmett Naughton <enaughton5@gmail.com>
2022-11-26 18:05:18 -0500

    first

---
+++ components/Blurb.jsx
@@ -0,0 +1,18 @@
+export default function Blurb() {
+  return (
+    <div>
+      <p>
+        Emmett is a software engineer with over five years of experience who
+        loves building incredible user experiences. He started in the world of
+        ecommerce — working with organziations such as Govalo, ecommerce
+        ecostystem, having worked with startups and shop merchants to bring
+        their products online.
+      </p>
+
+      <p>
+        He’s looking for his next opportunity as a developer, or solutions
+        engineer with a collaborative, remote team.
+      </p>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Blurb from "/components/Blurb";
 
 export default function Home() {
   return (
@@ -12,44 +13,36 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className={styles.title}>Welcome! </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          <Blurb />
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="https://emmettnaughton.com" className={styles.card}>
+            <h2>Read </h2>
+            <p>My writing about tech. </p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+            <h2>Listen </h2>
+            <p>Podcasts I've been on</p>
+          </a>
+
+          <a href="https://github.com/enaughton" className={styles.card}>
+            <h2>Learn </h2>
+            <p>Discover my projects.</p>
           </a>
 
           <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href="https://hireemmett.work"
             target="_blank"
             rel="noopener noreferrer"
             className={styles.card}
           >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <h2>Delight</h2>
+            <p>What people are saying</p>
           </a>
         </div>
       </main>
@@ -60,9 +53,14 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            <Image
+              src="/vercel.svg"
+              alt="Twitter Logo"
+              width={72}
+              height={16}
+            />
           </span>
         </a>
       </footer>
