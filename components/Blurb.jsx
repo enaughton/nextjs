ecfaec8fcd4d0972e2bbe13cbde78254aac48e08
Emmett Naughton <enaughton5@gmail.com>
2022-12-08 21:38:32 -0500

    Update Blurb.jsx

--- components/Blurb.jsx
+++ components/Blurb.jsx
@@ -4,7 +4,7 @@ export default function Blurb() {
       <p>
         Emmett is a software engineer with over five years of experience who
         loves building incredible user experiences. He started in the world of
-        ecommerce — working with organziations such as Govalo, ecommerce
+        ecommerce — working with organizations such as Govalo, ecommerce
         ecosystem, having worked with startups and shop merchants to bring their
         products online.
       </p>
